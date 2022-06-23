7094cffd38c3ee5fafceb30137d0978f2ed8decf
chentitorupa <ruizvicente020@gmail.com>
2022-06-22 22:15:06 -0500

    Se realizó ejercicio teórico y práctico de mensajes en consola.

---
+++ Práctica/Ejercicio-01/Ejercicio01/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+/*
+
+1. En este ejercicio, vamos a trabajar con el manejo de
+mensajes por pantalñla e introducción de información por
+teclado. Tenemos que realizar lo siguiente:
+
+a. Introducir por teclado el nombre, apellidos, edad y 
+una afición, y almacenarlo.
+b- Mostrarlo en el siguiente dormato.
+
+====== INFORMACIÓN BÁSICA =========
+Eres <nombre><apellidos>.
+Tienes <años> años.
+Hobby: <hobby>.
+===================================
+*/
+
+namespace Ejercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Dame tu nombre: ");
+            string name = Console.ReadLine();
+            Console.Write("Dame tus apellidos: ");
+            string lastname = Console.ReadLine();
+            Console.Write("Dame tu edad: ");
+            string age = Console.ReadLine();
+            Console.Write("¿Cuál es tu hobby?: ");
+            string hobby = Console.ReadLine();
+        
+            Console.WriteLine("====== INFORMACIÓN BÁSICA =========");
+            Console.WriteLine($"Eres {name} {lastname}.");
+            Console.WriteLine($"Tienes {age} años.");
+            Console.WriteLine($"Hobby: {hobby}.");
+            Console.WriteLine("===================================");
+
+        }
+    }
+}
---
+++ Teoría/2-Manejo-Mensajes-Pantalla/MensajePant/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MensajePant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bienvenido! ");
+            Console.Write("Chente Ruiz");
+
+            // Trabajando con la función WriteLine
+            string name = "Chente";
+            string lastname = "Ruiz";
+            Console.WriteLine("Trabajando con WriteLine()");
+            Console.WriteLine("Hola " + name);
+            Console.WriteLine("Hola " + name + " " + lastname);
+            Console.WriteLine("String Interpolation");
+            Console.WriteLine($"Hola soy {name} {lastname}");
+
+            // Trabajando con la función ReadLine()
+            /* Ejemplo 1*/
+            Console.WriteLine("Dime tu nombre:");
+            Console.WriteLine("Hola " + Console.ReadLine());
+            Console.WriteLine("Dime tu edad");
+            string edad = Console.ReadLine();
+            Console.WriteLine($"Tienes {edad}");
+            /* Ejemplo 2*/
+            Console.WriteLine("Dame un número");
+            // Primero lee el valor y después imprime en pantalla.
+            Console.WriteLine($"El número es: {Console.ReadLine()}");
+        }
+    }
+}
